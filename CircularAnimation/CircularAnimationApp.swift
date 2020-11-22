05b02a9829ebcf2ab259ed1deeae7d2c0056e5bf
Everton Carneiro <everton.mercante@gmail.com>
2020-11-22 11:13:01 -0300

    Initial Commit

---
+++ CircularAnimation/CircularAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularAnimationApp.swift
+//  CircularAnimation
+//
+//  Created by Everton Carneiro on 22/11/20.
+//
+
+import SwiftUI
+
+@main
+struct CircularAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircularAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CircularAnimation
+//
+//  Created by Everton Carneiro on 22/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
